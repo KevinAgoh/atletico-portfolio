59105e2de5387314ea69baf311c488ae23df2e29
Jules Cochard <jules.cochard@walterre.fr>
2024-09-16 18:50:54 +0200

    bio

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -4,11 +4,7 @@ import './bio.css';
 
 const Bio = () => {
   const bio =
-    `Atletico is composed of 3 people. They met a long time ago on Panorama
-          Bar’s dancefloor and have been playing music together since then.
-          While sharing a broad common base in terms of taste, each member
-          infuses their own touch, enabling them to deliver eclectic, dynamic
-          sets.`.split(' ');
+    `Atletico is composed of 3 people. They fell in love a long time ago on the dancefloor when the blind opened up on a sunny sunday morning. They have been digging and playing music together since then. While sharing a broad common base in terms of taste, each member infuses their own touch, enabling them to deliver eclectic and dynamic sets`.split(' ');
 
   return (
     <div id='bio'>
