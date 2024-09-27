1adc22a75bad19356e2fa1ecb3957a30fb806c7f
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2024-09-27 20:35:41 +0200

    Clean favicons path and remove unused js

--- src/constants/bio.js
+++
@@ -1,2 +0,0 @@
-export const BIO =
-  'Atletico is composed of 3 people. They fell in love a long time ago on the dancefloor when the blind opened up on a sunny sunday morning. They have been digging and playing music together since then. While sharing a broad common base in terms of taste, each member infuses their own touch, enabling them to deliver eclectic and dynamic sets';
