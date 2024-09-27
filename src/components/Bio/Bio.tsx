d8d111d8c1014a25c3c072ab9e7b6382bcbd2fee
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2024-09-27 20:58:50 +0200

    Fix string

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -4,7 +4,7 @@ import './bio.css';
 
 const Bio = () => {
   const bio =
-    `Atletico is composed of 3 friends. They fell in love a long time ago on the dancefloor when the blinds opened up on a unday morning. They have been digging and playing music together since then. While sharing a broad common base in terms of taste, each member infuses their own touch, enabling them to deliver eclectic and dynamic sets`.split(' ');
+    `Atletico is composed of 3 friends. They fell in love a long time ago on the dancefloor when the blinds opened up on a Sunday morning. They have been digging and playing music together since then. While sharing a broad common base in terms of taste, each member infuses their own touch, enabling them to deliver eclectic and dynamic sets`.split(' ');
 
   return (
     <div id='bio'>
