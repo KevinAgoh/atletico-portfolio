7beca50c6df98a49c106f0822376698b4fdffa9a
Jules Cochard <jules.cochard@walterre.fr>
2024-11-09 19:12:46 +0100

    fix link

--- src/components/Dates/dateEntries.js
+++ src/components/Dates/dateEntries.js
@@ -1,43 +1,43 @@
 export const dateEntries = [
-    {
-      year: 2024,
-      events: [
-        { month: 'Dec', event: 'Renate', location: 'Berlin, DE' }
-      ]
-    },
-    {
-      year: 2023,
-      events: [
-        { month: 'Nov', event: 'Watergate', location: 'Berlin, DE' },
-        { month: 'Oct', event: 'Crack Bellmer', location: 'Berlin, DE' },
-        { month: 'Oct', event: 'Bus Terraza', location: 'Barcelona, SP' },
-        { month: 'Jul', event: 'Watergate', location: 'Berlin, DE' },
-        { month: 'May', event: 'Radiant Act', location: 'Château de Marcoux, FR' },
-        { month: 'May', event: 'Le Groom', location: 'Lyon, FR' },
-        { month: 'Apr', event: 'Crack Bellmer', location: 'Berlin, DE' },
-        { month: 'Apr', event: 'Zur Klappe', location: 'Berlin, DE' },
-        { month: 'Mar', event: 'Watergate', location: 'Berlin, DE' },
-        { month: 'Mar', event: 'Club Opera', location: 'Morzine, FR' }
-      ]
-    },
-    {
-      year: 2022,
-      events: [
-        { month: 'Dec', event: 'Watergate', location: 'Berlin, DE' },
-        { month: 'Oct', event: 'Crack Bellmer', location: 'Berlin, DE' },
-        { month: 'Aug', event: 'Watergate', location: 'Berlin DE' },
-        { month: 'Jul', event: 'Café Borely', location: 'Marseille, FR' },
-        { month: 'Jun', event: 'ÆDEN', location: 'Berlin DE' },
-        { month: 'May', event: 'Radiant Act', location: 'Château de Buoux, FR' }
-      ]
-    },
-    {
-      year: 2021,
-      events: [
-        { month: 'Dec', event: 'HVLV', location: 'Kyiv, UA' },
-        { month: 'Nov', event: 'Christa Kupfer', location: 'Berlin, DE' },
-        { month: 'Sep', event: 'Crack Bellmer', location: 'Berlin, DE' }
-      ]
-    }
-  ];
-  
+  {
+    year: 2024,
+    events: [
+      { month: 'Dec', event: 'Renate', location: 'Berlin, DE' },
+      { month: 'Aug', event: 'Riviera', location: 'Malmöe, SW' }
+    ]
+  },
+  {
+    year: 2023,
+    events: [
+      { month: 'Nov', event: 'Watergate', location: 'Berlin, DE' },
+      { month: 'Oct', event: 'Crack Bellmer', location: 'Berlin, DE' },
+      { month: 'Oct', event: 'Bus Terraza', location: 'Barcelona, SP' },
+      { month: 'Jul', event: 'Watergate', location: 'Berlin, DE' },
+      { month: 'May', event: 'Radiant Act', location: 'Château de Marcoux, FR' },
+      { month: 'May', event: 'Le Groom', location: 'Lyon, FR' },
+      { month: 'Apr', event: 'Crack Bellmer', location: 'Berlin, DE' },
+      { month: 'Apr', event: 'Zur Klappe', location: 'Berlin, DE' },
+      { month: 'Mar', event: 'Watergate', location: 'Berlin, DE' },
+      { month: 'Mar', event: 'Club Opera', location: 'Morzine, FR' }
+    ]
+  },
+  {
+    year: 2022,
+    events: [
+      { month: 'Dec', event: 'Watergate', location: 'Berlin, DE' },
+      { month: 'Oct', event: 'Crack Bellmer', location: 'Berlin, DE' },
+      { month: 'Aug', event: 'Watergate', location: 'Berlin DE' },
+      { month: 'Jul', event: 'Café Borely', location: 'Marseille, FR' },
+      { month: 'Jun', event: 'ÆDEN', location: 'Berlin DE' },
+      { month: 'May', event: 'Radiant Act', location: 'Château de Buoux, FR' }
+    ]
+  },
+  {
+    year: 2021,
+    events: [
+      { month: 'Dec', event: 'HVLV', location: 'Kyiv, UA' },
+      { month: 'Nov', event: 'Christa Kupfer', location: 'Berlin, DE' },
+      { month: 'Sep', event: 'Crack Bellmer', location: 'Berlin, DE' }
+    ]
+  }
+];
